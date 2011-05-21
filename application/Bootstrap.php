b8c78c167b1eeefabee1715d8e5830855aacedb1
fernando.calo.sanchez@gmail.com <fernando.calo.sanchez@gmail.com@116a28d5-aabb-0dcf-45d8-469e69de473a>
2011-05-21 17:21:39 +0000

    Inicio

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,58 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    
+    protected function _initDoctype()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->setEncoding('UTF-8');
+        $view->doctype('XHTML1_STRICT');
+
+        ZendX_JQuery::enableView($view);
+
+    }
+
+    protected function _initAutoload()
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+                        'namespace' => '',
+                        'basePath' => APPLICATION_PATH));
+
+        return $moduleLoader;
+    }
 
 
+    protected function _initPlugins()
+    {
+        /*Zend_Controller_Action_HelperBroker::addPath( APPLICATION_PATH .'/controllers/helpers');
+
+        $front = Zend_Controller_Front::getInstance();
+
+        //init the routes
+        $router = $front->getRouter ();
+
+        //set the language route url (the default also)
+        $routeLang = new Zend_Controller_Router_Route ( ':language/:controller/:action/*', array ('language' => null, 'controller' => 'index', 'action' => 'index', 'module' =>'default' ) );
+
+        //set the download file page route
+        $routeDownload = new Zend_Controller_Router_Route( ':language/download/:id/*', array( 'language' => null, 'controller' => 'download', 'action' => 'file') );
+
+        //set the vote page route
+        $routeVote = new Zend_Controller_Router_Route( ':language/vote/:action/:id/:type/*', array( 'language' => null, 'controller' => 'vote', 'action' => 'file') );
+
+        //set the user profile route
+        $routeProfile = new Zend_Controller_Router_Route( ':language/profile/:username', array( 'language' => null, 'controller' => 'user', 'action' => 'profile') );
+
+        //set the api route
+        $routeApi = new Zend_Controller_Router_Route('/api/:action/*', array(  'controller' => 'api', 'action' => 'index') );
+
+        $router->addRoute ( 'default', $routeLang );//important, put the default route first!
+
+        //set all routes
+        $front->setRouter ( $router );
+
+        return $front;*/
+    }
 }
 
