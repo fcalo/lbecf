36a6b61d7e7868f2cf0ed7a1b45122f7fde1be6b
fernando.calo.sanchez@gmail.com <fernando.calo.sanchez@gmail.com@116a28d5-aabb-0dcf-45d8-469e69de473a>
2011-05-21 17:21:39 +0000

    Inicio

--- trunk/application/controllers/IndexController.php
+++ trunk/application/controllers/IndexController.php
@@ -6,14 +6,14 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $this->view->headScript()->appendFile( '/js/home.js');
     }
 
     public function indexAction()
     {
         // action body
-        $users = new Application_Model_DbTable_Users();
-        $this->view->users = $users->fetchAll();
-        var_dump($this->view->users);
+        //$users = new Application_Model_DbTable_Users();
+        //$this->view->users = $users->fetchAll();
     }
 
 
