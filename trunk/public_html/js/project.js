f980fab1884aa3b49b08897ef3df0dae2f91cfa1
fernando.calo.sanchez@gmail.com <fernando.calo.sanchez@gmail.com@116a28d5-aabb-0dcf-45d8-469e69de473a>
2011-06-10 21:51:46 +0000

    funcionalidad de apoyo

--- trunk/public_html/js/project.js
+++ trunk/public_html/js/project.js
@@ -12,9 +12,56 @@ $(function() {
         $("#recompensas .btn-red").click(function(){
             if($("#idUser").length==0 || $("#idUser").val()=="")
                 location.href="/usuario/login";
-            else
-                location.href="/apoyo/pago/"+($("#link").val())+"/"+($(this).attr("id"));
+            else{
+                var a=$(this).attr("id").split("_");
+                $("#id-recompensa").val(a[0]);
+                $("#apoyo-minimo").val(a[1]);
+                $("#amount").val($("#apoyo-minimo").val());
+
+                //TODO: aqui hay que pillar el id de recompensa y la cantidad minima
+                //Controlarlo en el dialogo y tambien almacenar la recompensa seleccionada en support
+                
+                $( "#dialog-form" ).dialog( "open" );
+
+                
+                //location.href="/apoyo/pago/"+($("#link").val())+"/"+($(this).attr("id"));
+            }
         });
 
 
+        $( "#dialog-form" ).dialog({
+			autoOpen: false,
+			height: 210,
+			width: 350,
+			modal: true,
+			buttons: {
+				"Apoyar": function() {
+					var bValid = true;
+					$("#amount").removeClass( "ui-state-error" );
+
+					bValid=(($("#amount").val()*1)>=($("#apoyo-minimo").val()*1));
+
+					if ( bValid ) {
+                                                $("#dialog-form div").hide();
+                                                $("#dialog-form").css("background","url(../img/loader.gif) center center no-repeat");
+						setTimeout('location.href="/apoyo/pago/'+($("#link").val())+'/'+($("#amount").val())+'/'+$("#id-recompensa").val()+'"',1000);
+						//$( this ).dialog( "close" );
+					}else{
+                                            $("#amount").addClass( "ui-state-error" );
+                                            $(".validateTips").html("Al menos debes aportar "+$("#apoyo-minimo").val()+" € para obtener la recompensa seleccionada")
+                                        }
+				},
+				"Cerrar": function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				$("#amount").removeClass( "ui-state-error" );
+			}
+		});
+
+                $("#amount").keyup(function(){
+                    if ($(this).val() != '')
+                        $(this).val($(this).attr('value').replace(/[^0-9]/g, ""));
+                });
 });
